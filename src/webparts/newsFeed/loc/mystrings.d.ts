1c1dfa4f043da98aff3eeb34ac3f5cbcdd53fd5d
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2020-05-14 21:08:10 +0300

    Initial commit

---
+++ src/webparts/newsFeed/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface INewsFeedWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'NewsFeedWebPartStrings' {
+  const strings: INewsFeedWebPartStrings;
+  export = strings;
+}
